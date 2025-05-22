fd71de8c2398f79f6e6c158283e08e61d3340b57
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-22 20:52:20 +0530

    Initial commit

---
+++ youtube/src/components/VideoCard.tsx
@@ -0,0 +1,15 @@
+export function VideoCard() {
+  return (
+    <div>
+      <img src="/photo.jpg"></img>
+      <div className="grid grid-cols-12">
+        <div className="grid-span-2">
+          <img src="/channel.jpg"></img>
+        </div>
+        <div className="grid-span-10">
+          TRUMP JUST DESTROYED 
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ youtube/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import {VideoCard} from "../components/VideoCard";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <VideoCard>
+        
+      </VideoCard>  
+    </div>
+  );
+}
+
