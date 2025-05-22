96b71e8d371e556e721ecbde902573da15f5e9be
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-22 21:10:52 +0530

    Initial commit

--- youtube/src/components/VideoCard.tsx
+++ youtube/src/components/VideoCard.tsx
@@ -1,13 +1,20 @@
 export function VideoCard() {
   return (
     <div>
-      <img src="/photo.jpg"></img>
-      <div className="grid grid-cols-12">
-        <div className="grid-span-2">
-          <img src="/channel.jpg"></img>
+      <img src="/photo.jpg" className="rounded-xl"></img>
+      <div className="grid grid-cols-12 pt-2">
+        <div className="col-span-1">
+          <img src="/channel.jpg" className={"rounded-full w-10 h-10"}></img>
         </div>
-        <div className="grid-span-10">
-          TRUMP JUST DESTROYED 
+        <div className="col-span-11 pl-5">
+          <div className="col-span-11 pl-5">
+            TRUMP JUST DESTROYED SOUTH AFRICA'S PRESIDENT | Absolute Cinema of
+            Geopolitics!
+          </div>
+
+          <div className="col-span-11 pl-5 text-gray-400">
+            878,322 views 10 hours ago
+          </div>
         </div>
       </div>
     </div>
