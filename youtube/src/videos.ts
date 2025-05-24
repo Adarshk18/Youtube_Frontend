42e04811a36bf235133464f5ade90d816e1bc8f8
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-24 13:12:02 +0530

    new

--- youtube/src/videos.ts
+++ youtube/src/videos.ts
@@ -8,73 +8,47 @@ export interface Video {
 
 export const VIDEOS: Video[] = [
     {
-        title: "New song",
+        title: "Introduction to DP",
         thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
+        description: "DP 1. Introduction to Dynamic Programming | Memoization | Tabulation | Space Optimization Techniques",
+        viewCount: "5.3k",
         timestamp: "4 hours ago"
     },
     {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
+        title: "100x devs",
+        thumbnail: "/video2.jpg",
+        description: "Your Humble Beginnings Should Not Stop You | How He Got a Remote Job at 19",
+        viewCount: "4k",
+        timestamp: "10 days ago"
     },
     {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
+        title: "England vs India",
+        thumbnail: "/video3.jpg",
+        description: "England Bowled Out For 183 | England v India - Day 1 Highlights | 1st LV= Insurance Test 2021",
+        viewCount: "19M",
+        timestamp: "3 years ago"
     },
     {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
+        title: "Free Blockchain Development Course",
+        thumbnail: "/video4.jpg",
+        description: "Code a Memecoin Launchpad like Pump.fun Step-by-Step (solidity, ethereum, hardhat, react js",
+        viewCount: "20k",
+        timestamp: "4 months ago"
     },
     {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
+        title: "Java + DSA",
+        thumbnail: "/video5.jpg",
+        description: "Best Data Structures & Algorithms (DSA) Course - Clear Any FAANG Interview!",
+        viewCount: "3.9M",
+        timestamp: "3 years ago"
     },
     {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
+        title: "Remote Jobs",
+        thumbnail: "/video6.avif",
+        description: "16 & 19 Year olds Earning more than 1 Crore as Remote Software Engineer🚀 | Remote Engineer in 2024",
+        viewCount: "173k",
+        timestamp: "1 years ago"
     },
-    {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
-    },
-    {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
-    },
-    {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
-    },
-    {
-        title: "New song",
-        thumbnail: "/video1.jpg",
-        description: "Mix Song of 2023 with dance and singing it in full stream",
-        viewCount: "1.3k",
-        timestamp: "4 hours ago"
-    }
+    
+   
 ]
